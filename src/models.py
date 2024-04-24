90bcb8a3509669a8d6c23b290613b642866a636b
DreiSup <153184780+DreiSup@users.noreply.github.com>
2024-04-24 19:36:08 +0000

    ig database model by drei

--- src/models.py
+++ src/models.py
@@ -7,23 +7,77 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_name = Column(String(25), nullable=False)
+    password = Column(String(100), nullable=False)
+    email= Column(String(35), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Photo(Base):
+    __tablename__ = 'photo'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    description = Column(String(40), nullable=False)
+    comments = Column(String(50), nullable=False)
+    likes = Column(Integer)
+    user_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship(Post)
+
+class Reel(Base):
+    __tablename__ = 'reel'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    description = Column(String(40), nullable=False)
+    comments = Column(String(50), nullable=False)
+    likes = Column(Integer)
+    user_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship(Post)
+
+class Story(Base):
+    __tablename__ = 'story'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    description = Column(String(40), nullable=False)
+    messages = Column(String(50), nullable=False)
+    likes = Column(Integer)
+    user_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship(Post)
+
+class Following(Base):
+    __tablename__ = 'following'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Followers(Base):
+    __tablename__ = 'followers'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+
 
     def to_dict(self):
         return {}
